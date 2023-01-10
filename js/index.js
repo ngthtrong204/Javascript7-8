dc7f326d29a6c8f98e31143750577a72038c8786
ngthtrong204 <ngthtrong204@gmail.com>
2023-01-10 13:08:49 +0700

    input array main

---
+++ js/index.js
@@ -0,0 +1,5 @@
+let mainArray = document.getElementById("mainArray");
+// if (mainArray.innerHTML) { } else {
+//     mainArray.classList.remove("bg-dark")
+// }
+mainArray.innerText ? {} : mainArray.classList.remove("bg-dark")
