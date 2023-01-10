6ab69c8f8af13f7adcb16d7cd4b99cdbef2032de
ngthtrong204 <ngthtrong204@gmail.com>
2023-01-10 13:18:44 +0700

    abc

--- js/index.js
+++ js/index.js
@@ -2,4 +2,4 @@ let mainArray = document.getElementById("mainArray");
 // if (mainArray.innerHTML) { } else {
 //     mainArray.classList.remove("bg-dark")
 // }
-mainArray.innerText ? {} : mainArray.classList.remove("bg-dark")
+mainArray.innerText ? {} : mainArray.classList.remove("bg-dark");
